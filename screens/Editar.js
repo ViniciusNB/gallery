76bc5ce53cf38464fb6b0384569d77685a477cb4
PatriciaRainha <patricia.s.rainha@gmail.com>
2022-04-13 21:01:01 -0300

    estilização página edição

--- screens/Editar.js
+++ screens/Editar.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, Button , StyleSheet } from "react-native";
 
 export default function Editar({ route, navigation }) {
   const { id } = route.params;
@@ -22,14 +22,14 @@ export default function Editar({ route, navigation }) {
       });
   }
   return (
-    <View>
-      <Text>Editar Page</Text>
-      <View>
+    <View style={styles.container}>
+      <View style={styles.conteudo}>
         {/* DIV TÍTULO */}
-        <View>
-          <Text style={{ fontSize: 25 }}>Título</Text>
+        <View style={styles.title}>
+        <Text style={{ fontSize: 25, marginBottom: 2 }}>Título</Text>
           <TextInput
             placeholder="  Minha Bela Imagem"
+            style={styles.inputs}
             onChange={(e) => {
               setTitle(e.target.value);
             }}
@@ -37,10 +37,11 @@ export default function Editar({ route, navigation }) {
         </View>
 
         {/* DIV IMAGEM */}
-        <View>
-          <Text style={{ fontSize: 25 }}>URL da imagem</Text>
+        <View style={styles.image}>
+        <Text style={{ fontSize: 25, marginBottom: 2 }}>URL da imagem</Text>
           <TextInput
             placeholder="  https://www.google.com"
+            style={styles.inputs}
             onChange={(e) => {
               setImage(e.target.value);
             }}
@@ -48,16 +49,82 @@ export default function Editar({ route, navigation }) {
         </View>
 
         {/* DIV DESCRIÇÃO */}
-        <View>
-          <Text style={{ fontSize: 25 }}>Descrição</Text>
+        <View style={styles.desc}>
+        <Text style={{ fontSize: 25, marginBottom: 7 }}>Descrição</Text>
           <TextInput
+            multiline={true}
+            style={styles.inputDesc}
             onChange={(e) => {
               setDescription(e.target.value);
             }}
           ></TextInput>
         </View>
-        <Button title="Editar" onPress={handleEditPress} />
+        
       </View>
+
+      <Button title="Editar" onPress={handleEditPress} />
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+
+    backgroundColor: "#fff",
+
+    margin: 0,
+    padding: 0,
+  },
+  conteudo: {
+    marginTop: 31,
+  },
+  add: {
+    backgroundColor: "#0066FF",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+
+    paddingLeft: 49,
+    paddingRight: 49,
+    paddingTop: 15,
+    paddingBottom: 15,
+
+    borderRadius: 50,
+  },
+  aviso: {
+    fontStyle: "normal",
+    fontWeight: "100",
+    fontSize: 18,
+    paddingBottom: 33,
+    lineHeight: 29.3,
+
+    textAlign: "center",
+
+    color: "#201E1E",
+  },
+  inputs: {
+    borderWidth: 1,
+    borderColor: "#BBBB",
+
+    width: 364,
+    height: 40,
+    borderRadius: 15,
+  },
+  inputDesc: {
+    borderWidth: 1,
+    borderColor: "#BBBB",
+    width: 364,
+    height: 309,
+    borderRadius: 15,
+  },
+  image: {
+    paddingTop: 23,
+  },
+  desc: {
+    paddingTop: 23,
+    paddingBottom: 54,
+  },
+});
